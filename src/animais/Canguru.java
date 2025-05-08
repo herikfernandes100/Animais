83606d5fedff3b3cd5dfa7ec7e81e6145ae91899
Herik <Herik@Herik>
2025-05-08 10:54:15 -0300

    Projeto Animais em Java

---
+++ src/animais/Canguru.java
@@ -0,0 +1,12 @@
+package animais;
+public class Canguru extends Mamifero{
+    // Métodos públicos
+    public void usarBolsa(){
+        System.out.println("Usando bolsa");
+    }
+    @Override
+    public void locomover(){
+        System.out.println("Saltando");
+    }
+    
+}
